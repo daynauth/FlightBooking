bc8632fd032621df992be282913df275f54fcac4
Roland Daynauth <daynauth@gmail.com>
2018-03-26 23:35:04 +0100

    Final update before submission

--- src/App.java
+++ src/App.java
@@ -1,12 +1,10 @@
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import Controllers.simulation.SimulationController;
 import models.Airport;
 import models.Booking;
 import models.CSVReader;
 import models.simulation.AirportSimulator;
 import singleton.TaskLogger;
-import views.MainGUI;
 import views.simulation.SimulationGUI;
 
 public class App {
@@ -19,37 +17,49 @@ public class App {
 	        }
 	    }, "Shutdown-thread"));
 		
-		startSimulation();
-		//MainGUI gui = new MainGUI();
-		//MainController controller = new MainController(airport, gui);
-		//controller.start();
+		//set the number of threads for sign in desks
+		int numThreads = 3;
+		
+		//start the simulation
+		startSimulation(numThreads);
 	}
 	
+	/**
+	 * Stage 1
+	 */
+	public static void SignIn() {
+		//MainGUI gui = new MainGUI();
+		//MainController controller = new MainController(airport, gui);
+		//controller.start();	
+	}
 	
-	public static void startSimulation() {
+	/**
+	 * Stage 2
+	 * @param num
+	 */
+	public static void startSimulation(int num) {
+		
 		//create the airport class and load the data from the csv file
 		Airport airport = new Airport();
 		CSVReader.loadFlights(airport);
 		
+		//create the global blocking queue for producer and consumer
 		ArrayBlockingQueue<Booking> queue = new ArrayBlockingQueue<Booking>(airport.getNumPassengers());
+		
+		//airport simulator adds passengers to the queue at random intervals
 		AirportSimulator a = new AirportSimulator(airport, queue);
 		
-
+		//initialize the ui
+		SimulationGUI simUI = new SimulationGUI(num, airport.getFlights().size());
 		
-		SimulationGUI simUI = new SimulationGUI(3, airport.getFlights().size());
-		
-		
+		//create and start the producer thread
 		Thread t = new Thread(a);
+		
+		//start the controller
 		SimulationController simController = new SimulationController(simUI, airport, queue);
 		a.AddObserver(simController);
-		
 		t.start();
-		
-		
-		
 		simController.start();	
-		
-		
 	}
 
 }
--- src/threads/DeskThread.java
+++
@@ -1,5 +0,0 @@
-package threads;
-
-public interface DeskThread {
-
-}
